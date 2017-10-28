4bcacfc48c183deb1509d545c55d7d20c609c216
Vitaliy <newboxemail@gmail.com>
2017-10-28 21:33:36 +0300

    ready courses page

--- archive-cours.php
+++ archive-cours.php
@@ -11,13 +11,146 @@
 
 				<div class="col-md-4">
 					<div class="item_course">
-						<div class="img_block" style="background:url('<?php bloginfo('template_url') ?>/build/images/course_img.jpg') no-repeat center center; background-size:cover;">
-							<a href="#"><img src="<?php bloginfo('template_url') ?>/build/images/course_img.png" alt=""></a>
+						<div class="top_block" style="background:url('<?php bloginfo('template_url') ?>/build/images/course_img.jpg') no-repeat center center; background-size:cover;">
+							<div class="top_info">
+								<div class="places_count">Places: 3</div>
+								<div class="info_start">
+									<span class="start">Start</span>
+									<div class="date">
+										<span class="day">12</span>
+										<span class="mouth">June</span>
+										<span class="year">2018</span>
+									</div>
+								</div>
+							</div>
 						</div>
 						<div class="inner_item_course">
 							<h2 class="title_item_post"><a href="">English for children</a></h2>
 							<div class="meta_block_info">
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, ipsum suscipit, nisi atque ut modi ex perferendis, temporibus tempora architecto unde reiciendis velit, illum similique doloremque nihil aspernatur voluptatum magnam.</p>
+								<div class="item_meta"><span>Level:</span> elementary</div>
+								<div class="item_meta"><span>People in group:</span> 3</div>
+								<div class="item_meta"><span>Duration:</span> 16 lessons 60 min 2 mounths 2 times week</div>
+								<div class="item_meta price"><span>Price:</span> 96$</div>
+							</div>
+							<div class="btn_wrap">
+								<a href="" class="btn_default">Read more</a>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-4">
+					<div class="item_course">
+						<div class="top_block" style="background:url('<?php bloginfo('template_url') ?>/build/images/course_img.jpg') no-repeat center center; background-size:cover;">
+							<div class="top_info">
+								<div class="places_count">Places: 3</div>
+								<div class="info_start">
+									<span class="start">Start</span>
+									<div class="date">
+										<span class="day">12</span>
+										<span class="mouth">June</span>
+										<span class="year">2018</span>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="inner_item_course">
+							<h2 class="title_item_post"><a href="">English for children</a></h2>
+							<div class="meta_block_info">
+								<div class="item_meta"><span>Level:</span> elementary</div>
+								<div class="item_meta"><span>People in group:</span> 3</div>
+								<div class="item_meta"><span>Duration:</span> 16 lessons 60 min 2 mounths 2 times week</div>
+								<div class="item_meta price"><span>Price:</span> 96$</div>
+							</div>
+							<div class="btn_wrap">
+								<a href="" class="btn_default">Read more</a>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-4">
+					<div class="item_course">
+						<div class="top_block" style="background:url('<?php bloginfo('template_url') ?>/build/images/course_img.jpg') no-repeat center center; background-size:cover;">
+							<div class="top_info">
+								<div class="places_count">Places: 3</div>
+								<div class="info_start">
+									<span class="start">Start</span>
+									<div class="date">
+										<span class="day">12</span>
+										<span class="mouth">June</span>
+										<span class="year">2018</span>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="inner_item_course">
+							<h2 class="title_item_post"><a href="">English for children</a></h2>
+							<div class="meta_block_info">
+								<div class="item_meta"><span>Level:</span> elementary</div>
+								<div class="item_meta"><span>People in group:</span> 3</div>
+								<div class="item_meta"><span>Duration:</span> 16 lessons 60 min 2 mounths 2 times week</div>
+								<div class="item_meta price"><span>Price:</span> 96$</div>
+							</div>
+							<div class="btn_wrap">
+								<a href="" class="btn_default">Read more</a>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-4">
+					<div class="item_course">
+						<div class="top_block" style="background:url('<?php bloginfo('template_url') ?>/build/images/course_img.jpg') no-repeat center center; background-size:cover;">
+							<div class="top_info">
+								<div class="places_count">Places: 3</div>
+								<div class="info_start">
+									<span class="start">Start</span>
+									<div class="date">
+										<span class="day">12</span>
+										<span class="mouth">June</span>
+										<span class="year">2018</span>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="inner_item_course">
+							<h2 class="title_item_post"><a href="">English for children</a></h2>
+							<div class="meta_block_info">
+								<div class="item_meta"><span>Level:</span> elementary</div>
+								<div class="item_meta"><span>People in group:</span> 3</div>
+								<div class="item_meta"><span>Duration:</span> 16 lessons 60 min 2 mounths 2 times week</div>
+								<div class="item_meta price"><span>Price:</span> 96$</div>
+							</div>
+							<div class="btn_wrap">
+								<a href="" class="btn_default">Read more</a>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-4">
+					<div class="item_course">
+						<div class="top_block" style="background:url('<?php bloginfo('template_url') ?>/build/images/course_img.jpg') no-repeat center center; background-size:cover;">
+							<div class="top_info">
+								<div class="places_count">Places: 3</div>
+								<div class="info_start">
+									<span class="start">Start</span>
+									<div class="date">
+										<span class="day">12</span>
+										<span class="mouth">June</span>
+										<span class="year">2018</span>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="inner_item_course">
+							<h2 class="title_item_post"><a href="">English for children</a></h2>
+							<div class="meta_block_info">
+								<div class="item_meta"><span>Level:</span> elementary</div>
+								<div class="item_meta"><span>People in group:</span> 3</div>
+								<div class="item_meta"><span>Duration:</span> 16 lessons 60 min 2 mounths 2 times week</div>
+								<div class="item_meta price"><span>Price:</span> 96$</div>
 							</div>
 							<div class="btn_wrap">
 								<a href="" class="btn_default">Read more</a>
@@ -27,76 +160,26 @@
 				</div>
 				<div class="col-md-4">
 					<div class="item_course">
-						<div class="img_block" style="background:url('<?php bloginfo('template_url') ?>/build/images/course_img.jpg') no-repeat center center; background-size:cover;">
-							<a href="#"><img src="<?php bloginfo('template_url') ?>/build/images/course_img.png" alt=""></a>
+						<div class="top_block" style="background:url('<?php bloginfo('template_url') ?>/build/images/course_img.jpg') no-repeat center center; background-size:cover;">
+							<div class="top_info">
+								<div class="places_count">Places: 3</div>
+								<div class="info_start">
+									<span class="start">Start</span>
+									<div class="date">
+										<span class="day">12</span>
+										<span class="mouth">June</span>
+										<span class="year">2018</span>
+									</div>
+								</div>
+							</div>
 						</div>
 						<div class="inner_item_course">
 							<h2 class="title_item_post"><a href="">English for children</a></h2>
 							<div class="meta_block_info">
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, ipsum suscipit, nisi atque ut modi ex perferendis, temporibus tempora architecto unde reiciendis velit, illum similique doloremque nihil aspernatur voluptatum magnam.</p>
-							</div>
-							<div class="btn_wrap">
-								<a href="" class="btn_default">Read more</a>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-4">
-					<div class="item_course">
-						<div class="img_block" style="background:url('<?php bloginfo('template_url') ?>/build/images/course_img.jpg') no-repeat center center; background-size:cover;">
-							<a href="#"><img src="<?php bloginfo('template_url') ?>/build/images/course_img.png" alt=""></a>
-						</div>						<div class="inner_item_course">
-							<h2 class="title_item_post"><a href="">English for children</a></h2>
-							<div class="meta_block_info">
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, ipsum suscipit, nisi atque ut modi ex perferendis, temporibus tempora architecto unde reiciendis velit, illum similique doloremque nihil aspernatur voluptatum magnam.</p>
-							</div>
-							<div class="btn_wrap">
-								<a href="" class="btn_default">Read more</a>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-4">
-					<div class="item_course">
-						<div class="img_block" style="background:url('<?php bloginfo('template_url') ?>/build/images/course_img.jpg') no-repeat center center; background-size:cover;">
-							<a href="#"><img src="<?php bloginfo('template_url') ?>/build/images/course_img.png" alt=""></a>
-						</div>
-						<div class="inner_item_course">
-							<h2 class="title_item_post"><a href="">English for children</a></h2>
-							<div class="meta_block_info">
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, ipsum suscipit, nisi atque ut modi ex perferendis, temporibus tempora architecto unde reiciendis velit, illum similique doloremque nihil aspernatur voluptatum magnam.</p>
-							</div>
-							<div class="btn_wrap">
-								<a href="" class="btn_default">Read more</a>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-4">
-					<div class="item_course">
-						<div class="img_block" style="background:url('<?php bloginfo('template_url') ?>/build/images/course_img.jpg') no-repeat center center; background-size:cover;">
-							<a href="#"><img src="<?php bloginfo('template_url') ?>/build/images/course_img.png" alt=""></a>
-						</div>
-						<div class="inner_item_course">
-							<h2 class="title_item_post"><a href="">English for children</a></h2>
-							<div class="meta_block_info">
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, ipsum suscipit, nisi atque ut modi ex perferendis, temporibus tempora architecto unde reiciendis velit, illum similique doloremque nihil aspernatur voluptatum magnam.</p>
-							</div>
-							<div class="btn_wrap">
-								<a href="" class="btn_default">Read more</a>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-4">
-					<div class="item_course">
-						<div class="img_block" style="background:url('<?php bloginfo('template_url') ?>/build/images/course_img.jpg') no-repeat center center; background-size:cover;">
-							<a href="#"><img src="<?php bloginfo('template_url') ?>/build/images/course_img.png" alt=""></a>
-						</div>
-						<div class="inner_item_course">
-							<h2 class="title_item_post"><a href="">English for children</a></h2>
-							<div class="meta_block_info">
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, ipsum suscipit, nisi atque ut modi ex perferendis, temporibus tempora architecto unde reiciendis velit, illum similique doloremque nihil aspernatur voluptatum magnam.</p>
+								<div class="item_meta"><span>Level:</span> elementary</div>
+								<div class="item_meta"><span>People in group:</span> 3</div>
+								<div class="item_meta"><span>Duration:</span> 16 lessons 60 min 2 mounths 2 times week</div>
+								<div class="item_meta price"><span>Price:</span> 96$</div>
 							</div>
 							<div class="btn_wrap">
 								<a href="" class="btn_default">Read more</a>
