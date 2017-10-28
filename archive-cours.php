a515d5adc7ca55b349cf297225c388ec821ac9e8
Vitaliy <newboxemail@gmail.com>
2017-10-28 19:36:25 +0300

    fix underlin

---
+++ archive-cours.php
@@ -0,0 +1,113 @@
+<?php get_header('page') ?>
+
+<?php get_template_part('components/breadcrumbs'); ?>
+
+<?php get_template_part_with_data('components/category-description'); ?>
+
+<div class="main_container">
+	<div class="container">
+		<div class="wrap_courses">
+			<div class="row">
+
+				<div class="col-md-4">
+					<div class="item_course">
+						<div class="img_block" style="background:url('<?php bloginfo('template_url') ?>/build/images/course_img.jpg') no-repeat center center; background-size:cover;">
+							<a href="#"><img src="<?php bloginfo('template_url') ?>/build/images/course_img.png" alt=""></a>
+						</div>
+						<div class="inner_item_course">
+							<h2 class="title_item_post"><a href="">English for children</a></h2>
+							<div class="meta_block_info">
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, ipsum suscipit, nisi atque ut modi ex perferendis, temporibus tempora architecto unde reiciendis velit, illum similique doloremque nihil aspernatur voluptatum magnam.</p>
+							</div>
+							<div class="btn_wrap">
+								<a href="" class="btn_default">Read more</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="item_course">
+						<div class="img_block" style="background:url('<?php bloginfo('template_url') ?>/build/images/course_img.jpg') no-repeat center center; background-size:cover;">
+							<a href="#"><img src="<?php bloginfo('template_url') ?>/build/images/course_img.png" alt=""></a>
+						</div>
+						<div class="inner_item_course">
+							<h2 class="title_item_post"><a href="">English for children</a></h2>
+							<div class="meta_block_info">
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, ipsum suscipit, nisi atque ut modi ex perferendis, temporibus tempora architecto unde reiciendis velit, illum similique doloremque nihil aspernatur voluptatum magnam.</p>
+							</div>
+							<div class="btn_wrap">
+								<a href="" class="btn_default">Read more</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="item_course">
+						<div class="img_block" style="background:url('<?php bloginfo('template_url') ?>/build/images/course_img.jpg') no-repeat center center; background-size:cover;">
+							<a href="#"><img src="<?php bloginfo('template_url') ?>/build/images/course_img.png" alt=""></a>
+						</div>						<div class="inner_item_course">
+							<h2 class="title_item_post"><a href="">English for children</a></h2>
+							<div class="meta_block_info">
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, ipsum suscipit, nisi atque ut modi ex perferendis, temporibus tempora architecto unde reiciendis velit, illum similique doloremque nihil aspernatur voluptatum magnam.</p>
+							</div>
+							<div class="btn_wrap">
+								<a href="" class="btn_default">Read more</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="item_course">
+						<div class="img_block" style="background:url('<?php bloginfo('template_url') ?>/build/images/course_img.jpg') no-repeat center center; background-size:cover;">
+							<a href="#"><img src="<?php bloginfo('template_url') ?>/build/images/course_img.png" alt=""></a>
+						</div>
+						<div class="inner_item_course">
+							<h2 class="title_item_post"><a href="">English for children</a></h2>
+							<div class="meta_block_info">
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, ipsum suscipit, nisi atque ut modi ex perferendis, temporibus tempora architecto unde reiciendis velit, illum similique doloremque nihil aspernatur voluptatum magnam.</p>
+							</div>
+							<div class="btn_wrap">
+								<a href="" class="btn_default">Read more</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="item_course">
+						<div class="img_block" style="background:url('<?php bloginfo('template_url') ?>/build/images/course_img.jpg') no-repeat center center; background-size:cover;">
+							<a href="#"><img src="<?php bloginfo('template_url') ?>/build/images/course_img.png" alt=""></a>
+						</div>
+						<div class="inner_item_course">
+							<h2 class="title_item_post"><a href="">English for children</a></h2>
+							<div class="meta_block_info">
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, ipsum suscipit, nisi atque ut modi ex perferendis, temporibus tempora architecto unde reiciendis velit, illum similique doloremque nihil aspernatur voluptatum magnam.</p>
+							</div>
+							<div class="btn_wrap">
+								<a href="" class="btn_default">Read more</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="item_course">
+						<div class="img_block" style="background:url('<?php bloginfo('template_url') ?>/build/images/course_img.jpg') no-repeat center center; background-size:cover;">
+							<a href="#"><img src="<?php bloginfo('template_url') ?>/build/images/course_img.png" alt=""></a>
+						</div>
+						<div class="inner_item_course">
+							<h2 class="title_item_post"><a href="">English for children</a></h2>
+							<div class="meta_block_info">
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, ipsum suscipit, nisi atque ut modi ex perferendis, temporibus tempora architecto unde reiciendis velit, illum similique doloremque nihil aspernatur voluptatum magnam.</p>
+							</div>
+							<div class="btn_wrap">
+								<a href="" class="btn_default">Read more</a>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div> 
+</div>
+
+<?php get_footer() ?>
--- include/custom_types.php
+++ include/custom_types.php
@@ -166,3 +166,83 @@ function register_post_teacher(){
 
 
 
+add_action('init', 'register_post_cours');
+function register_post_cours(){
+    register_post_type('cours', array(
+        'label'  => null,
+        'labels' => array(
+            'name'               => 'Курсы', // основное название для типа записи
+            'singular_name'      => 'Курсы', // название для одной записи этого типа
+            'add_new'            => 'Добавить курс', // для добавления новой записи
+            'add_new_item'       => 'Добавление курса', // заголовка у вновь создаваемой записи в админ-панели.
+            'edit_item'          => 'Редактирование курса', // для редактирования типа записи
+            'new_item'           => 'Новый курс', // текст новой записи
+            'view_item'          => 'Смотреть курс', // для просмотра записи этого типа.
+            'search_items'       => 'Искать курс', // для поиска по этим типам записи
+            'not_found'          => 'Не найдено', // если в результате поиска ничего не было найдено
+            'not_found_in_trash' => 'Не найдено в корзине', // если не было найдено в корзине
+            'parent_item_colon'  => '', // для родителей (у древовидных типов)
+            'menu_name'          => 'Курсы', // название меню
+        ),
+        'description'         => '',
+        'public'              => true,
+        'publicly_queryable'  => true,
+        'exclude_from_search' => false,
+        'show_ui'             => true, 
+        'show_in_menu'        => true, // показывать ли в меню адмнки
+        'show_in_admin_bar'   => null, // по умолчанию значение show_in_menu
+        'show_in_nav_menus'   => true,
+        'menu_position'       => 5,
+        'menu_icon'           => 'dashicons-welcome-learn-more',
+        //'capability_type'   => 'post',
+        //'capabilities'      => 'post', // массив дополнительных прав для этого типа записи
+        //'map_meta_cap'      => null, // Ставим true чтобы включить дефолтный обработчик специальных прав
+        'hierarchical'        => false,
+        'supports'            => array('title', 'editor',  'thumbnail','excerpt'), // 'title','editor','author','thumbnail','excerpt','trackbacks','custom-fields','comments','revisions','page-attributes','post-formats'
+        'taxonomies'          => array('course_cat', 'language'),
+        'has_archive'         => true,
+        'rewrite'             => true,
+        'query_var'           => true,
+    ) );
+}
+
+// хук для регистрации
+add_action('init', 'create_taxonomy_cat_course');
+function create_taxonomy_cat_course(){
+    // заголовки
+    $labels = array(
+        'name'              => 'Категори курсов',
+        'singular_name'     => 'Категори курсов',
+        'search_items'      => 'Искать категорию',
+        'all_items'         => 'Все категории',
+        'parent_item'       => 'Родительская категория',
+        'parent_item_colon' => 'Родительская категория:',
+        'edit_item'         => 'Редактировать категорию',
+        'update_item'       => 'Обновить категорию',
+        'add_new_item'      => 'Добавить категорию',
+        'new_item_name'     => 'Новое название категории',
+        'menu_name'         => 'Категории',
+    ); 
+    // параметры
+    $args = array(
+        'label'                 => '', // определяется параметром $labels->name
+        'labels'                => $labels,
+        'description'           => '', // описание таксономии
+        'public'                => true,
+        'publicly_queryable'    => true, // равен аргументу public
+        'show_in_nav_menus'     => true, // равен аргументу public
+        'show_ui'               => true, // равен аргументу public
+        'show_tagcloud'         => true, // равен аргументу show_ui
+        'hierarchical'          => true,
+        'update_count_callback' => '',
+        'rewrite'               => true,
+        //'query_var'             => $taxonomy, // название параметра запроса
+        'capabilities'          => array(),
+        'meta_box_cb'           => null, // callback функция. Отвечает за html код метабокса (с версии 3.8): post_categories_meta_box или post_tags_meta_box. Если указать false, то метабокс будет отключен вообще
+        'show_admin_column'     => false, // Позволить или нет авто-создание колонки таксономии в таблице ассоциированного типа записи. (с версии 3.5)
+        '_builtin'              => false,
+        'show_in_quick_edit'    => null, // по умолчанию значение show_ui
+    );
+    register_taxonomy('course_cat', array('cours'), $args );
+}
+
